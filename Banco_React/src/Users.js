8809fa65d93cedb848209f9334ec379bcabd8812
RuanRCA <assuncaoruan7@gmail.com>
2025-08-27 08:02:03 -0300

    .

--- Banco_React/src/Users.js
+++ Banco_React/src/Users.js
@@ -1,13 +1,31 @@
 import {View , StyleSheet , Text , TouchableOpacity} from "react-native";
+import {db} from './Firebase.Connections';
+import {deleteDoc , doc} from "firebase/firestore";
 
-export function UsersList({data}){
+export function UsersList({data , handlerEdit}){
+
+   async function handlerDeletarItem(){
+        // console.log(data)
+        const docRef = doc(db,"Users" , data.id)
+        await deleteDoc(docRef);
+    }
+
+    function handlerEditUser(){
+        //console.log(data)
+        handlerEdit(data)
+
+    }
     return(
     <View style={styles.container}>
      <Text>Nome: {data.nome}</Text>
      <Text>Idade:{data.idade}</Text>
      <Text>Cargo:{data.cargo}</Text>
-     <TouchableOpacity style={styles.botao}>
+     <TouchableOpacity style={styles.botao} onPress={handlerDeletarItem}>
         <Text style={styles.texto}>Deletar Usuário</Text>
+     </TouchableOpacity>
+
+     <TouchableOpacity style={styles.butonEdit} onPress={handlerEditUser}>
+        <Text style={styles.texto}>Editar Usuário</Text>
      </TouchableOpacity>
     </View>
     )
@@ -32,8 +50,22 @@ botao:{
      borderWidth:2,
      margin:10,
      marginLeft:250,
-     marginTop:-38
+     marginTop:38
 },
+
+butonEdit:{
+    padding:10,
+     width:75,
+     borderRadius:10,
+     borderColor:'black',
+     height:60,
+     backgroundColor:'green',
+     borderWidth:2,
+     margin:15,
+     marginrigth:250,
+     marginTop:-70
+},
+
 texto:{
     fontWeight:'bold',
     color:'#fff'
